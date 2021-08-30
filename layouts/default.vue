9f0043c9fba8759a3ab9be6bccb6527c226e7f2f
Elise Maschke <elise.maschke@outlook.de>
2021-08-30 10:41:49 +0200

    working layouts

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>THE STORE</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+
+html {
+  background-color: #ffff7b;
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.5;
+  margin: 10px;
+  text-align: center;
+}
+</style>
---
+++ layouts/products.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>THE PRODUCTS</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+html {
+  background-color: #b6ff7b;
+  
+}
+
+</style>
